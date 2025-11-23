6c60e22554fe7f0f0362362e2e27ce06e159e49b
MarkBacsi <FaustianAuthor@protonmail.com>
2025-11-23 23:58:17 +1100

    Deleted duplicate code and added to csproj so that net will run

--- src/Algorithms/QuickSort.cs
+++
@@ -1,8 +0,0 @@
-namespace AlgorithmSorter.Algorithms
-{
-    public interface ISortingAlgorithm
-    {
-        string Name { get; }
-        void Sort(int[] array, Action<int[], int, int>? visualizationCallback = null);
-    }
-}
